c039e9addc8cf7674bf11e920ea15eb92493717b
HugoSanchezGallego <hsancgal@myuax.com>
2024-11-30 13:33:16 +0100

    hecho

--- src/main/java/com/example/microservicio3/Microservicio3Application.java
+++ src/main/java/com/example/microservicio3/Microservicio3Application.java
@@ -1,13 +1,12 @@
 package com.example.microservicio3;
 
 import com.example.microservicio3.Schedulers.MasterScheduler;
+import com.example.microservicio3.models.SensorDataRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,12 +15,16 @@ public class Microservicio3Application {
     @Autowired
     private MasterScheduler masterScheduler;
 
+    @Autowired
+    private SensorDataRepository sensorDataRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Microservicio3Application.class, args);
     }
 
     @PostConstruct
     public void init() {
+        sensorDataRepository.deleteAll();
         masterScheduler.startMonitoringFlows();
     }
 }
